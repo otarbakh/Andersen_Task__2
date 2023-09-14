3913441c300175bc07e9111d67414796e9b6cdc0
otarbakh <otarbakh@gmail.com>
2023-09-14 21:48:55 +0400

    moving forward

--- app/src/main/java/com/otarbakh/andersen_task__2/ui/fragments/ContactsDetailsFragment.kt
+++ app/src/main/java/com/otarbakh/andersen_task__2/ui/fragments/ContactsDetailsFragment.kt
@@ -1,11 +1,17 @@
 package com.otarbakh.andersen_task__2.ui.fragments
 
 
+import android.content.res.Configuration
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.otarbakh.andersen_task__2.R
+import com.otarbakh.andersen_task__2.common.Constants.nameBundleKey
+import com.otarbakh.andersen_task__2.common.Constants.numberBundleKey
+import com.otarbakh.andersen_task__2.common.Constants.surnameBundleKey
+import com.otarbakh.andersen_task__2.data.model.ContactsDetail
 import com.otarbakh.andersen_task__2.databinding.ContactDetailsFragmentBinding
 import com.otarbakh.andersen_task__2.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,58 +26,84 @@ class ContactsDetailsFragment :
     override fun viewCreated() {
 
 
+        getContactDetailsOnLandscape()
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.getPhoneNumber().collectLatest {
-                    binding.tvPhone.text = it.phoneNumber
-                    binding.tvName.text = it.name
-                    binding.tvSurname.text = it.surname
-                }
-            }
+
+        val name = arguments?.getString(nameBundleKey)
+        val surname = arguments?.getString(surnameBundleKey)
+        val number = arguments?.getString(numberBundleKey)
+
+        binding.apply {
+
+            tvPhone.text = number
+            tvName.text = name
+            tvSurname.text = surname
         }
 
+    }
 
-//        val name = arguments?.getString(nameBundleKey)
-//        val surname = arguments?.getString(surnameBundleKey)
-//        val number = arguments?.getString(numberBundleKey)
-//
-//        binding.apply {
-//
-//            tvPhone.text = number
-//            tvName.text = name
-//            tvSurname.text = surname
-//        }
+    private fun getContactDetailsOnLandscape() {
+        if (context != null) {
+            val isLandscape =
+                resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
+            if (isLandscape) {
+
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                            viewModel.getPhoneNumber().collectLatest {data ->
+                                binding.tvPhone.text = data.phoneNumber
+                                binding.tvName.text = data.name
+                                binding.tvSurname.text = data.surname
+                                binding.save.setOnClickListener{
+                                    viewModel.updateContact(
+                                        ContactsDetail(
+                                            data.id,
+                                            data.name,
+                                            data.surname,
+                                            data.phoneNumber
+                                        )
+                                    )
+                                }
+
+                            }
+
+                        }
+                    }
+
+
+            } else {
+                updateContact()
+            }
+        }
     }
 
 
     private fun updateContact() {
-//        val receiveID = arguments?.getInt("id")
-//
-//        binding.save.setOnClickListener {
-//            viewLifecycleOwner.lifecycleScope.launch {
-//                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-//                    viewModel.updateContact(
-//                        contact = ContactsDetail(
-//                            receiveID!!,
-//                            name = binding.editName.text.toString(),
-//                            surname = binding.ediSurname.text.toString(),
-//                            phoneNumber = binding.editNumber.text.toString()
-//                        )
-//                    )
-//
-//                }
-//            }
-//            val transaction = parentFragmentManager.beginTransaction()
-//            transaction.replace(R.id.fullScreen_fragment, ContactsFragment())
-//            transaction.commit()
-//        }
+        val receiveID = arguments?.getInt("id")
+
+        binding.save.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    viewModel.updateContact(
+                        contact = ContactsDetail(
+                            receiveID!!,
+                            name = binding.editName.text.toString(),
+                            surname = binding.ediSurname.text.toString(),
+                            phoneNumber = binding.editNumber.text.toString()
+                        )
+                    )
+                }
+            }
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fullScreen_fragment, ContactsFragment())
+            transaction.commit()
+        }
     }
 
 
     override fun listeners() {
-        updateContact()
+
 
     }
 }
